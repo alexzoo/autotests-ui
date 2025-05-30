5d4a417e5324370e7dcc3825c22770e4f26ade13
Alexey Zubov <zubov.alexey@gmail.com>
2025-05-30 17:36:05 +0200

    added test_edit_course

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -43,3 +43,41 @@ class TestCourses:
         courses_list_page.course_view.check_visible(
             title="Playwright", index=0, max_score="100", min_score="10", estimated_time="2 weeks"
         )
+
+    def test_edit_course(
+        self,
+        create_course_page: CreateCoursePage,
+        courses_list_page: CoursesListPage,
+    ):
+        create_course_page.visit(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
+        )
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10",
+        )
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            title="Playwright", index=0, max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+
+        courses_list_page.course_view.menu.click_edit(0)
+        create_course_page.create_course_form.check_visible(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10",
+        )
+        create_course_page.create_course_form.fill(
+            title="New title", estimated_time="1 week", description="Cypress", max_score="50", min_score="20"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            title="New title", index=0, max_score="50", min_score="20", estimated_time="1 week"
+        )
