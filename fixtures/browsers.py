090050eb60f1658392033382e2a0f84fe26b6e99
Alexey Zubov <zubov.alexey@gmail.com>
2025-06-04 20:08:20 +0200

    added tracer to Allure reports

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,14 +1,23 @@
+import allure
 import pytest
+from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:  # type: ignore
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:  # type: ignore
     browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()  # type: ignore
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield context.new_page()  # type: ignore
+
+    context.tracing.stop(path=f"./tracing/{request.node.name}.zip")
     browser.close()
+
+    allure.attach.file(f"./tracing/{request.node.name}.zip", name="trace", extension="zip")
 
 
 @pytest.fixture(scope="session")
@@ -28,27 +37,19 @@ def initialize_browser_state(playwright: Playwright) -> None:
     )
     registration_page.click_registration_button()
 
-    # page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    # email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    # email_input.fill("user.name@gmail.com")
-
-    # username_input = page.get_by_test_id("registration-form-username-input").locator("input")
-    # username_input.fill("username")
-
-    # password_input = page.get_by_test_id("registration-form-password-input").locator("input")
-    # password_input.fill("password")
-
-    # registration_button = page.get_by_test_id("registration-page-registration-button")
-    # registration_button.click()
-
     context.storage_state(path="browser-state.json")
     browser.close()
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:  # type: ignore
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:  # type: ignore
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
     yield context.new_page()  # type: ignore
+
+    context.tracing.stop(path=f"./tracing/{request.node.name}.zip")
     browser.close()
+
+    allure.attach.file(f"./tracing/{request.node.name}.zip", name="trace", extension="zip")
