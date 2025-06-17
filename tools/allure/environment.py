7df332b33530727ea78eadb998ff0947c3933fa6
Alexey Zubov <zubov.alexey@gmail.com>
2025-06-17 22:08:19 +0200

    added to environment os and python versions

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,9 +1,22 @@
 from config import settings
+import platform
+import sys
+
 
 
 def create_allure_environment_file():
+    os_info = f"{platform.system()}, {platform.release()}"
+    python_version = sys.version
+
     items = [f"{key}={value}" for key, value in settings.model_dump().items()]
-    properties = "\n".join(items)
+    environment_items = [
+        f"os_info={os_info}",
+        f"python_version={python_version}"
+    ]
+
+    all_items = items + environment_items
+    properties = "\n".join(all_items)
 
     with open(settings.allure_results_dir.joinpath("environment.properties"), "w+") as file:
         file.write(properties)
+
