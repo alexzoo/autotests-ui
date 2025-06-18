a4099b6d35ef48b8c19968079ef8eff1f4f11bd7
Alexey Zubov <zubov.alexey@gmail.com>
2025-06-18 10:41:42 +0200

    fixes by review

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,22 +1,15 @@
-from config import settings
 import platform
 import sys
 
+from config import settings
 
 
 def create_allure_environment_file():
-    os_info = f"{platform.system()}, {platform.release()}"
-    python_version = sys.version
+    items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    items.append(f'os_info={platform.system()}, {platform.release()}')
+    items.append(f'python_version={sys.version}')
 
-    items = [f"{key}={value}" for key, value in settings.model_dump().items()]
-    environment_items = [
-        f"os_info={os_info}",
-        f"python_version={python_version}"
-    ]
+    properties = '\n'.join(items)
 
-    all_items = items + environment_items
-    properties = "\n".join(all_items)
-
-    with open(settings.allure_results_dir.joinpath("environment.properties"), "w+") as file:
+    with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties)
-
